e7cba2160d1a3e3e15384087152f4728c6ac8495
Tonja Joseph <tonjaj@stud.ntnu.no>
2020-08-10 11:06:57 +0200

    Implementation of malfunction_check-table

--- MalfunctionIndicator/DetectDefectEquipment.m
+++ MalfunctionIndicator/DetectDefectEquipment.m
@@ -22,8 +22,6 @@ treadmills = unique(data.label);
 % Day of interest
 curr_day_data = fetchDay(data, '08', '02', '2020');
 
-
-
 % One day earlier
 ode_data = fetchDay(data, '07', '02', '2020');
 
@@ -34,10 +32,10 @@ owe_exercise_hrs = sum(owe_data.motion);
 curr_day_sum_exercise_hrs = 0;
 
 usage_comparison = cell2table(cell(length(treadmills),7));
-usage_comparison.Properties.VariableNames = {'label', 'curr_day_usage', 'owe_usage', 'ode_usage', 'usage_diff_owe', 'usage_diff_ode', 'malfunction'};
+usage_comparison.Properties.VariableNames = {'label', 'curr_day_usage', 'owe_usage', 'ode_usage', 'usage_diff_owe', 'usage_diff_ode', 'max_continuity'};
 
-malfunction_check = cell2table(cell(length(treadmills),4);
-malfunction_check.Properties.VariableNames = {'average', 'one_week_earlier', 'one_day_earlier', 'continuity'};
+malfunction_check = cell2table(cell(length(treadmills),7));
+malfunction_check.Properties.VariableNames = {'label', 'usage_level', 'average', 'one_week_earlier', 'one_day_earlier', 'continuity', 'sum'};
 
 for i = 1:length(treadmills)
    
@@ -72,62 +70,70 @@ for i = 1:length(treadmills)
         usage_comparison.usage_diff_ode{i} = 100;
     end
     
-%     avg_usage_diff = sum(usage_comparison.usage_diff) / length(treadmills);
-%     
-%     if ( (tm_usage_diff < -0.9) && ((avg_usage_diff - tm_usage_diff) < 0.2) )
-%         
-%          usage_comparison.malfunction{i} = true;
-%          
-%     else
-%          
-%         usage_comparison.malfunction{i} = false;
-%     end
+    max_count = 0;
+    count = 0;
+    for j = 1:length(tm_curr_day_data.motion)
+        if (tm_curr_day_data.motion(j) == 1)
+            count = count + 1;
+        elseif (tm_curr_day_data.motion(j) == 0) 
+            if (count > max_count)
+                max_count = count;
+            end
+            count = 0;
+        end
+    end
+    
+    usage_comparison.max_continuity{i} = max_count;
     
 end
 
 curr_day_avg_usage = sum(cell2mat(usage_comparison.curr_day_usage)) / length(treadmills);
 
 
-% % avg_usage_diff = sum(cell2mat(usage_comparison.usage_diff)) / length(treadmills);
-% 
+ 
 for i = 1:length(treadmills)
     
+    malfunction_check.label(i) = treadmills(i);
     
+    % Less than 0.5% usage
+    if (usage_comparison.curr_day_usage{i} < 0.0075)
+        malfunction_check.usage_level{i} = true;
+    else
+        malfunction_check.usage_level{i} = false;
+    end
+
+    % Less than 10% of average usage? 
     if (usage_comparison.curr_day_usage{i} < 0.1 * curr_day_avg_usage)
         malfunction_check.average{i} = true;
     else
         malfunction_check.average{i} = false;
     end
     
+    % More than 90 % reduction from week before?
     if (usage_comparison.usage_diff_owe{i} < -0.9)
         malfunction_check.one_week_earlier{i} = true;
     else
         malfunction_check.one_week_earlier{i} = false;
     end 
     
-    if (usage_comparison.usage_diff_ode{i} < -0.7 || usage_comparison.ode_usage{i} < 0.0075) )
+    % More than 70% reduction from day before OR low usage level day before as
+    % well?
+    if (usage_comparison.usage_diff_ode{i} < -0.7 || usage_comparison.ode_usage{i} < 0.075 )
         malfunction_check.one_day_earlier{i} = true;
     else
         malfunction_check.one_day_earlier{i} = false;
-    end        
+    end      
     
-    
-    tm_usage_diff_owe = usage_comparison.usage_diff_owe{i};
-    tm_usage_diff_ode = usage_comparison.usage_diff_ode{i};
-    
-    if ( (tm_usage_diff_owe < -0.7) && (tm_usage_diff_ode < -0.5 || usage_comparison.ode_usage{i} < 0.020 ))  %&& ((avg_usage_diff - tm_usage_diff) < 0.2) )
-        
-         usage_comparison.malfunction{i} = true;
-         
+    % Less than 10 minutes of maximum continuous use?
+    if (usage_comparison.max_continuity{i} < 10)
+        malfunction_check.continuity{i} = true;
     else
-         
-        usage_comparison.malfunction{i} = false;
+        malfunction_check.continuity{i} = false;
     end
+    
+    malfunction_check.sum{i} = sum(cell2mat(malfunction_check{i,2:6}));
+    
+
 end
 
 
-curr_day_exercise_hrs = sum(curr_day_data.motion);
-
-% One week earlier
-owe_data = fetchDay(data, '04', '03', '2020');
-owe_exercise_hrs = sum(owe_data.motion);
